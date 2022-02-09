6f8c22bf278ba24bf1622a0a5a222f52e3a9873e
James Hager <james@avantiapplications.com>
2022-02-09 16:47:44 -0700

    Change the SWAPI get request to use a query instead of a param.

--- swapi/main.js
+++ swapi/main.js
@@ -4,10 +4,10 @@ document.getElementById('theButton').addEventListener('click', (event) => {
     // const newP = document.createElement('p')
     // newP.textContent = 'The button was clicked'
     // document.querySelector('body').appendChild(newP)
-    axios.get('https://swapi.dev/api/planets/2')
+    axios.get('https://swapi.dev/api/planets?search=Ald')
     .then((res) =>{
-        console.log(res.data.residents)
-        for (let resident of res.data.residents) {
+        console.log(res.data.results[0].residents)
+        for (let resident of res.data.results[0].residents) {
             console.log(resident)
             axios.get(resident)
             .then((res) => {
